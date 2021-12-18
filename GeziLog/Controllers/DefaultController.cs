ebd2ee109bbe0092c0d1173c723e70f09fdd69d4
KubraAkkaya <akkaya32kubra@gmail.com>
2021-12-18 16:31:47 +0300

    18.12.2021

--- GeziLog/Controllers/DefaultController.cs
+++ GeziLog/Controllers/DefaultController.cs
@@ -9,11 +9,22 @@ namespace GeziLog.Controllers
 {
     public class DefaultController : Controller
     {
+
+        Context contextBlogs = new Context();
+        BlogComment bc = new BlogComment();
         // GET: Index
 
         public ActionResult Index()
         {
-            return View();
+            bc.Value3 = contextBlogs.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();
+           // var values = contextBlogs.Blogs.ToList(); burası için de nasıl view de döndürebiliriz!
+            return View(bc);
+        }
+
+        public PartialViewResult Partial()
+        {
+            var values = contextBlogs.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();
+            return PartialView(values);
         }
 
         // GET: About
@@ -34,16 +45,17 @@ namespace GeziLog.Controllers
 
         // GET: Blogs
 
-        Context contextBlogs = new Context();
+
         public ActionResult Blogs()
         {
-            var values = contextBlogs.Blogs.ToList(); 
-            return View(values);
+            //var values = contextBlogs.Blogs.ToList(); 
+            bc.Value1 = contextBlogs.Blogs.ToList();
+            return View(bc);
         }
 
         // GET: BlogDetails
 
-        BlogComment bc = new BlogComment();
+        
         public ActionResult BlogDetails(int id)
         {
             //var findBlog = contextBlogs.Blogs.Where(x=>x.ID == id).ToList();
